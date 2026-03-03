021cd5bae4bc7da3e0a9b1836002e7242eb389f3
nakshjoshi <nakshjoshi2004@gmail.com>
2026-03-03 14:47:33 +0530

    multer middleware

---
+++ backend/src/middlewares/multer.middleware.ts
@@ -0,0 +1,16 @@
+import multer from "multer"
+
+const storage = multer.diskStorage({
+    destination: function(req, file, cb){
+        return cb(null, "./multer-uploads")
+    },
+    filename: function(req, file, cb){
+        const uniqueName = Date.now + "-"+ file.originalname
+        return cb(null, uniqueName)
+
+    }
+})
+
+const upload = multer({storage})
+
+export {upload}
