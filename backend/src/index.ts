a0f24308b579488c2f7115d10e781b2f47f557b9
nakshjoshi <nakshjoshi2004@gmail.com>
2026-03-03 12:36:58 +0530

    setting up basic HTTP server

---
+++ backend/src/index.ts
@@ -0,0 +1,15 @@
+import express from 'express'
+
+
+const port = process.env.PORT || 3001
+
+
+const app = express()
+
+app.get('/', (req,res)=>{
+    res.send('Hello HTTP')
+})
+
+app.listen(port, ()=>{
+    console.log(`App running on http://localhost:${port}`)
+})
