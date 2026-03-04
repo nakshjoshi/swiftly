3d88a9e98f18f43ca62b3bfe7e9413bcfa6cf18d
nakshjoshi <nakshjoshi2004@gmail.com>
2026-03-04 11:45:38 +0530

    async handler utils

---
+++ backend/src/utils/asyncHandler.utils.ts
@@ -0,0 +1,10 @@
+import type { NextFunction, Request, RequestHandler, Response } from "express";
+
+const asyncHandler = (fn:RequestHandler)=>{
+
+    return (req:Request, res:Response, next:NextFunction):void =>{
+        Promise
+            .resolve(fn(req, res, next))
+            .catch((err)=>next(err))
+    }
+}
