96b1743e8fbea1c7d02b70c5770a7bbb91fceba8
nakshjoshi <nakshjoshi2004@gmail.com>
2026-03-14 02:48:23 +0530

    add vercel analytics

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import Providers from "@/components/Providers";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -35,6 +36,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Analytics/>
         <Providers>
           <Navbar />
           {children}
